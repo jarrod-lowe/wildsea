43ffd058dae08fbd62b72b1976e43b2beab30947
Jarrod Lowe <github@rrod.net>
2024-08-03 19:19:41 +1200

    more test workflaow

--- terraform/environment/github/main.tf
+++ terraform/environment/github/main.tf
@@ -5,3 +5,17 @@ data "aws_region" "current" {}
 output "aws_region" {
     value = data.aws_region.current.id
 }
+
+variable "aws_account" {
+    type = string
+    sensitive = true
+}
+
+variable "aws_region" {
+    type = string
+    sensitive = true
+}
+
+provider "aws" {
+  
+}
