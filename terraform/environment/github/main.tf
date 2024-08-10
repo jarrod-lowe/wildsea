d1ee7cbf485ee5aa78313d2cc255c7ba1e82ab40
Jarrod Lowe <github@rrod.net>
2024-08-10 13:47:23 +1200

    more test workflaow

--- terraform/environment/github/main.tf
+++ terraform/environment/github/main.tf
@@ -24,8 +24,6 @@ variable "environment" {
 
 terraform {
     backend "s3" {
-        bucket = var.state_bucket
-        key = "${var.environment}/terraform.tfstate"
         region = var.aws_region
     }
 }
