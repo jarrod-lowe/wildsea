0695fa6575edef7361e3a93cb1924f082210463e
Jarrod Lowe <41766555+jarrod-lowe@users.noreply.github.com>
2024-08-10 14:15:18 +1200

    Environment (#7)
    * Testing branch protection
    
    * First workflow
    
    * First workflow - fixes
    
    * Pin versions
    
    * Environment
    
    * Create test workflaow
    
    * more test workflaow
    
    * more test workflaow
    
    * more test workflaow
    
    * more test workflaow
    
    * more test workflaow
    
    * more test workflaow
    
    * more test workflaow
    
    * more test workflaow
    
    * more test workflaow
    
    * more test workflaow
    
    * more test workflaow
    
    * more test workflaow
    
    * more test workflaow
    
    * more test workflaow
    
    * more test workflaow
    
    * more test workflaow
    
    * more test workflaow
    
    * more test workflaow
    
    * more test workflaow
    
    * more test workflaow
    
    * more test workflaow

--- terraform/environment/github/main.tf
+++ terraform/environment/github/main.tf
@@ -1,1 +1,35 @@
 resource "null_resource" "test" {}
+
+variable "aws_account" {
+    description = "ID of the AWS Account"
+    type = string
+    sensitive = true
+}
+
+variable "aws_region" {
+    description = "AWS Region name"
+    type = string
+    sensitive = true
+}
+
+variable "state_bucket" {
+    description = "Name of the S3 state bucket"
+    type        = string
+}
+
+variable "environment" {
+    description = "Name of the Environment"
+    type        = string
+}
+
+terraform {
+    backend "s3" {
+        // region, bucket and key come from -backend-config
+    }
+}
+
+provider "aws" {
+    assume_role {
+      role_arn = "arn:aws:iam::${var.aws_account}:role/GitHubAction-Wildsea"
+    }
+}
