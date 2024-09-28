bd5e637e5e6c6cb7affa1c7909014a156af431a9
Jarrod Lowe <github@rrod.net>
2024-09-28 21:06:29 +1200

    Use popovers with markdown instead of tooltips

--- ui/src/components/SectionItem.tsx
+++ ui/src/components/SectionItem.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Tippy from '@tippyjs/react';
+import ReactMarkdown from 'react-markdown';
+import 'tippy.js/dist/tippy.css';
 import { FaInfoCircle } from 'react-icons/fa';
-import { Tooltip } from 'react-tooltip';
 import { BaseSectionItem } from '../baseSection';
+import MDEditor, { getCommands, ICommand } from '@uiw/react-md-editor';
 
 interface SectionItemProps<T extends BaseSectionItem> {
   readonly item: T;
@@ -13,12 +16,47 @@ export function SectionItem<T extends BaseSectionItem>({ item, renderContent }: 
     <div className={`section-item ${item.constructor.name.toLowerCase()}-item`}>
       <span>{item.name}</span>
       {renderContent(item)}
-      <FaInfoCircle
-        className="info-icon"
-        data-tooltip-content={item.description}
-        data-tooltip-id={`description-tooltip-${item.id}`}
-      />
-      <Tooltip id={`description-tooltip-${item.id}`} place="top" />
+      { item.description && (
+      <Tippy
+        content={<ReactMarkdown>{item.description}</ReactMarkdown> }
+        interactive={true}
+        trigger="click"
+        arrow={true}
+        placement="top"
+        className="markdown-tippy"
+      >
+        <button className="info-icon-button">
+          <FaInfoCircle className="info-icon" />
+        </button>
+      </Tippy>
+      )}
     </div>
   );
 }
+
+interface MarkdownEditorProps {
+  value: string;
+  onChange: (value: string) => void;
+  placeholder?: string;
+}
+
+export const SectionItemDescription: React.FC<MarkdownEditorProps> = ({ value, onChange, placeholder }) => {
+  // Filter out the preview command to protect against XSS issues
+  const customCommands = getCommands().filter((cmd: ICommand) => 
+    cmd.name !== 'preview' && cmd.name !== 'live'
+  );
+
+  return (
+    <MDEditor
+      value={value}
+      onChange={(value) => onChange(value ?? "")}
+      preview="edit"
+      textareaProps={{placeholder: placeholder }}
+      commands={customCommands}
+      extraCommands={[]}
+      previewOptions={{
+        allowedElements: [],
+      }}
+    />
+  );
+};
