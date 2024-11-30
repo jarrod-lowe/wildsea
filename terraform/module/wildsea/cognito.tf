c3ce00d815c44680f50b765f08eca2cbf7c78463
Jarrod Lowe <github@rrod.net>
2024-11-30 17:22:25 +1300

    Push up oauth change

--- terraform/module/wildsea/cognito.tf
+++ terraform/module/wildsea/cognito.tf
@@ -122,9 +122,15 @@ resource "aws_cognito_identity_provider" "google-oauth" {
   provider_type = "Google"
 
   provider_details = {
-    authorize_scopes = "profile openid email"
-    client_id        = var.google_client_id
-    client_secret    = var.google_client_secret
+    authorize_scopes              = "profile openid email"
+    client_id                     = var.google_client_id
+    client_secret                 = var.google_client_secret
+    attributes_url                = "https://people.googleapis.com/v1/people/me?personFields="
+    attributes_url_add_attributes = true
+    authorize_url                 = "https://accounts.google.com/o/oauth2/v2/auth"
+    oidc_issuer                   = "https://accounts.google.com"
+    token_request_method          = "POST"
+    token_url                     = "https://www.googleapis.com/oauth2/v4/token"
   }
 
   attribute_mapping = {
