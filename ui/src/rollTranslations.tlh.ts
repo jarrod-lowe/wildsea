f2ad4edc45baf3ef4787cad1fe9b795d41030933
Jarrod Lowe <jarrod@lowe.kiwi>
2025-09-21 14:02:50 +1200

    Add missing Klingon

--- ui/src/rollTranslations.tlh.ts
+++ ui/src/rollTranslations.tlh.ts
@@ -8,10 +8,14 @@ export const diceRollMessagesKlingon = {
     "result.deltaGreenAttack.fumble.0": "{name} {action} Qagh! HIv Qagh.",
     "result.deltaGreenAttack.critical_success.0": "Qapchu'! {name} {action} HIvqu'.",
 
+    "result.deltaGreen.success.0": "Qap! {name} {action} Qapchu' {roll}.",
+    "result.deltaGreen.failure.0": "luj! {name} {action} luj {roll}.",
+    "result.deltaGreen.fumble.0": "Qagh! {name} {action} Qaghqu' {roll}.",
+    "result.deltaGreen.critical_success.0": "Qapchu'qu'! {name} {action} Qapqu' {roll}.",
+
     // Delta Green Damage
     "result.deltaGreenDamage.neutral.0": "{name} Heghlu'meH QaQ jajvam: {roll} QIH.",
     "result.deltaGreenDamage.neutral.1": "{name} QIH: {roll} Hegh.",
-
 
     // Neutral (no target, no action)
     "result.neutral.neutral.0": "{name} {roll} SuvwI'.",
